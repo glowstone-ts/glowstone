e3a943c5f7ed0d940a9ad51f6847ddbca6f19122
Khao <khao@khaodoes.dev>
2026-04-26 13:54:23 +0700

    refactor(codegen): make burger util use the already existing output.json

--- codegen/util/burger.ts
+++ codegen/util/burger.ts
@@ -1,19 +1,23 @@
 export async function getBurgerData(jar: string) {
-  console.log(`Generating burger data for ${jar}...`);
-  const proc = Bun.spawn({
-    cmd: ["uv", "run", "munch.py", "../../" + jar, "--output", "../../tmp/output.json"],
-    cwd: "codegen/azalea-burger",
-  });
+  const outputFile = Bun.file("tmp/output.json");
 
-  await proc.exited;
-  if (proc.exitCode != 0) {
-    throw new Error(`Code generation failed with exit code ${proc.exitCode}`);
+  if (!outputFile.exists()) {
+    console.log(`Generating burger data for ${jar}...`);
+    const proc = Bun.spawn({
+      cmd: ["uv", "run", "munch.py", "../../" + jar, "--output", "../../tmp/output.json"],
+      cwd: "codegen/azalea-burger",
+    });
+
+    await proc.exited;
+    if (proc.exitCode != 0) {
+      throw new Error(`Code generation failed with exit code ${proc.exitCode}`);
+    }
+    if (!outputFile.exists()) throw new Error("Output file does not exist (it should've generated?), please try again.");
+  } else {
+    console.log(`Burger data already generated for ${jar}, skipping...`);
   }
 
-  const outputFile = Bun.file("tmp/output.json");
-  if (!outputFile.exists()) throw new Error("Output file does not exist. Please run the code generation script first.");
-
   const output = await outputFile.json() as any[];
-  if (!Array.isArray(output) || output.length == 0) throw new Error("Output file is not in the expected format (array) or is empty.");
+  if (!Array.isArray(output) || output.length == 0) throw new Error("Output file is not in the expected format (array) or is empty, please try again.");
   return output[0];
 }
