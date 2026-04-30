527c9435faaa3f4fe353b9693d692ebd6a56502f
Khao <khao@khaodoes.dev>
2026-04-30 14:03:34 +0700

    refactor(protocol): make namespace a nullable inside KnownPack datatype

--- packages/protocol/src/datatypes/KnownPack.ts
+++ packages/protocol/src/datatypes/KnownPack.ts
@@ -1,26 +1,14 @@
 import type { PacketReader, PacketWriter } from "../buffer";
 
 export class KnownPack {
-  public namespace: string;
-  public id: string;
-  public version: string;
-
-  constructor({
-    namespace = "minecraft",
-    id, 
-    version
-  }: {
-    namespace?: string;
-    id: string;
-    version: string;
-  }) {
-    this.namespace = namespace;
-    this.id = id;
-    this.version = version;
-  }
+  constructor(
+    public namespace: string | null,
+    public id: string,
+    public version: string,
+  ) {}
 
   write(writer: PacketWriter) {
-    writer.writeString(this.namespace);
+    writer.writeString(this.namespace ?? "minecraft");
     writer.writeString(this.id);
     writer.writeString(this.version);
   }
@@ -29,6 +17,6 @@ export class KnownPack {
     const namespace = reader.readString();
     const id = reader.readString();
     const version = reader.readString();
-    return new KnownPack({ namespace, id, version });
+    return new KnownPack(namespace, id, version);
   }
 }
